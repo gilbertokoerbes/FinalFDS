a57d02e0fafad0bb92355561660b81a96b8e2341
Wolxf2 <luizjirkowsky19@outlook.com>
2021-07-03 20:20:19 -0300

    att caso de uso

---
+++ cidareira/src/main/java/com/finalfds/cidareira/aplicacao/Casos_de_uso_Comum/AtReclamacao.java
@@ -0,0 +1,21 @@
+package com.finalfds.cidareira.aplicacao.Casos_de_uso_Comum;
+
+import com.finalfds.cidareira.negocio.entidades.*;
+import com.finalfds.cidareira.negocio.servicos.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AtReclamacao {
+    private ServicoReclamacao servicoReclamacao;
+
+    @Autowired
+    public AtReclamacao(ServicoReclamacao servicoReclamacao){
+        this.servicoReclamacao = servicoReclamacao;
+    }
+
+    public Reclamacao run(Long id, String titulo, String descricao, String status, Usuario usuario, Categoria categoria, Endereco endereco){
+        return servicoReclamacao.
+    }
+}
